3a725f0a053a1e1d6ba36126f8e3462b5198b61f
mrmrs <hi@mrmrs.cc>
2018-04-11 19:35:01 +0200

    No such thing as font don't pass it in.

--- components/Div.js
+++ components/Div.js
@@ -1,7 +1,7 @@
 import glamorous from 'glamorous'
-import { space, width, font, fontSize, textAlign, color, borders, borderRadius } from 'styled-system'
+import { space, width, fontSize, textAlign, color, borders, borderRadius } from 'styled-system'
 
-const Div = glamorous.div(space, width, font, fontSize, color, textAlign, borders, borderRadius, {
+const Div = glamorous.div(space, width, fontSize, color, textAlign, borders, borderRadius, {
   boxSizing: 'border-box'
 })
 
