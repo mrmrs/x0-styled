f0cd854964d32c3bb7b641251baf92784fa7d6ac
mrmrs <hi@mrmrs.cc>
2018-04-23 12:55:18 +0200

    Add normalize css to HR by default

--- components/Hr.js
+++ components/Hr.js
@@ -2,7 +2,9 @@ import glamorous from 'glamorous'
 import { space, width, color, borders } from 'styled-system'
 
 const Hr = glamorous.hr(space, width, color, borders, {
-
+  boxSizing: 'content-box', 
+  height: 0, 
+  overflow: 'visible'
 })
 
 Hr.defaultProps = {
