e1052f75ebb5a8ec1f01c1ef7b7b8475c8816189
mrmrs <hi@mrmrs.cc>
2018-05-21 12:33:05 +0200

    Update the Div component to allow for maxWidth being set.

--- elements/Div.js
+++ elements/Div.js
@@ -1,7 +1,12 @@
 import glamorous from 'glamorous'
+
 import { space, width, display, fontSize, textAlign, color, borders, borderColor, borderWidth, borderRadius } from 'styled-system'
 
-const Div = glamorous.div(space, width, display, fontSize, color, textAlign, borders, borderColor, borderWidth, borderRadius, {
+const maxWidth = props => ({
+  maxWidth: props.maxWidth? props.maxWidth : '100%'
+})
+
+const Div = glamorous.div(space, width, display, fontSize, color, textAlign, borders, borderColor, borderWidth, borderRadius, maxWidth, {
   boxSizing: 'border-box'
 })
 
